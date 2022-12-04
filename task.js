49633e536ea80b031d35a9d8cfb43ce8f5cd46da
Lonskoy <lonskoy0304@gmail.com>
2022-12-05 02:16:05 +0300

    add files

---
+++ task.js
@@ -0,0 +1,24 @@
+let menu = document.getElementsByClassName('menu__item');
+arrMenu = Array.from(menu);
+let cash = [];
+
+arrMenu.forEach((elem) => {                   //Критерий поиска - вложенность. Если вложенность есть, то это не ссылка.
+                                              //По чему не активны ссылки во вложенных меню?
+
+    if(elem.children.length > 1) {
+        elem.onclick = function() {
+            if(cash.length === 0) {
+                cash.push(elem.children[1]);
+                elem.children[1].className = 'menu menu_sub menu_active';
+                return false;
+            }
+            else {
+                cash[0].className = 'menu menu_sub';
+                cash[0] = elem.children[1];
+                elem.children[1].className = 'menu menu_sub menu_active';
+                return false;
+            }
+        }
+    }
+});
+
